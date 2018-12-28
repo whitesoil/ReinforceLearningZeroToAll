2bbd4db8c0cc94f77df2fbfc3893c4bb66df3ecf
whitesoil <whitesoil.kr@gmail.com>
2018-12-28 18:01:33 +0900

    lab7

--- lab7-2.py
+++ lab7-2.py
@@ -1,4 +1,4 @@
-# DQN with replay
+# DQN with replay, targetDQN
 
 import numpy as np
 import tensorflow as tf
@@ -16,22 +16,33 @@ output_size = env.action_space.n
 dis = 0.9
 REPLAY_MEMORY = 50000
 
-def simple_replay_train(DQN, train_batch):
-    x_stack = np.empty(0).reshape(0, DQN.input_size)
-    y_stack = np.empty(0).reshape(0, DQN.output_size)
+def replay_train(mainDQN,targetDQN, train_batch):
+    x_stack = np.empty(0).reshape(0, input_size)
+    y_stack = np.empty(0).reshape(0, output_size)
 
     for state, action, reward, next_state, done in train_batch:
-        Q = DQN.predict(state)
+        Q = mainDQN.predict(state)
 
         if done:
             Q[0, action] = reward
         else:
-            Q[0,action] = reward + dis * np.max(DQN.predict(next_state))
+            Q[0,action] = reward + dis * np.max(targetDQN.predict(next_state))
         
         y_stack = np.vstack([y_stack, Q])
         x_stack = np.vstack([x_stack, state])
 
-    return DQN.update(x_stack, y_stack)
+    return mainDQN.update(x_stack, y_stack)
+
+def get_copy_var_ops(*, dest_scope_name="target", src_scope_name="main"):
+    op_holder = []
+
+    src_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
+    dest_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
+
+    for src_var, dest_var in zip(src_vars, dest_vars):
+        op_holder.append(dest_var.assign(src_var.value()))
+
+    return op_holder
 
 def bot_play(mainDQN):
     s = env.reset()
@@ -51,8 +62,12 @@ def main():
     replay_buffer = deque()
 
     with tf.Session() as sess:
-        mainDQN = dqn.DQN(sess, input_size, output_size)
+        mainDQN = dqn.DQN(sess, input_size, output_size, name="main")
+        targetDQN = dqn.DQN(sess, input_size, output_size, name="target")
+
         tf.global_variables_initializer().run()
+
+        copy_ops = get_copy_var_ops(dest_scope_name="target", src_scope_name="main")
 
         for episode in range(max_episodes):
             e = 1. / ((episode / 10) + 1)
@@ -87,8 +102,9 @@ def main():
                 if episode % 10 == 1:
                     for _ in range(50):
                         minibatch = random.sample(replay_buffer, 10)
-                        loss, _ = simple_replay_train(mainDQN, minibatch)
+                        loss, _ = replay_train(mainDQN, targetDQN, minibatch)
                     print("Loss: ", loss)
+                    sess.run(copy_ops)
                 
             bot_play(mainDQN)
 
