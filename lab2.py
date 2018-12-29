bc23ffc935e851647670361382cc80229613831b
whitesoil <whitesoil.kr@gmail.com>
2018-12-29 13:33:46 +0900

    Lab2 해설추가

--- lab2.py
+++ lab2.py
@@ -4,7 +4,7 @@ import gym
 from gym.envs.registration import register
 import sys, tty, termios
 
-
+# Keyborad 입력 처리
 class _Getch:
 	def __call__(self):
 		fd = sys.stdin.fileno()
@@ -15,6 +15,7 @@ class _Getch:
 		finally:
 			termios.tcsetattr(fd,termios.TCSADRAIN, old_settings)
 		return ch
+
 inkey = _Getch()
 
 LEFT = 0
@@ -28,13 +29,17 @@ arrow_keys = {
 	'\x1b[C' : RIGHT,
 	'\x1b[D' : LEFT}
 
+# OpenAI Gym의 게임 환경 셋팅
 register(
 	id='FrozenLake-v3',
 	entry_point='gym.envs.toy_text:FrozenLakeEnv',
 	kwargs={'map_name' : '4x4', 'is_slippery': False}
 )
 
+# 게임 환경 생성
 env = gym.make('FrozenLake-v3')
+
+# 게임을 Graphic으로 렌더링
 env.render()
 
 while True:
@@ -44,6 +49,15 @@ while True:
 		break
 
 	action = arrow_keys[key]
+
+	"""
+		Action에 따른 결과값 반환
+		
+		state : 현재 위치
+		reward : 보상
+		done : 게임이 끝났는지 여부
+		Info : 게임 정보
+	"""
 	state, reward, done, info = env.step(action)
 	env.render()
 	print("S: ", state, "Action: ", action, "Reward: ", reward, "Info: ",info)
