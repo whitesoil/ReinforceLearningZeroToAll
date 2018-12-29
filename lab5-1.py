00fc31afd58bfb2eeca14e679de1bf43f2fc276c
whitesoil <whitesoil.kr@gmail.com>
2018-12-29 14:27:46 +0900

    lab5 해설 추가

--- lab5-1.py
+++ lab5-1.py
@@ -1,7 +1,9 @@
-import gym
+# Non-deterministic world
 
+import gym
 import readchar
 
+# Keyborad 입력을 Gym 코드와 바인딩
 LEFT = 0
 DOWN = 1
 RIGHT = 2
@@ -14,19 +16,34 @@ arrow_keys = {
 	'\x1b[D' : LEFT
     }
 
+# FrozenLake-v0 는 기본적으로 slippery가 On 되어있음, 게임 환경 생성
 env = gym.make('FrozenLake-v0')
+
+# 게임을 Graphic으로 렌더링
 env.render()
+
+# 현재 위치 초기화
 state = env.reset()
 
 while True :
+	# lab2.py 와 다른 방식으로 키 입력을 받음
 	key = readchar.readkey()
 	if key not in arrow_keys.keys():
 		print("Game aborted")
 		break
 
+	"""
+		Action에 따른 결과값 반환
+		
+		state : 현재 위치
+		reward : 보상
+		done : 게임이 끝났는지 여부
+		Info : 게임 정보
+	"""
 	action = arrow_keys[key]
 	state, reward, done, info = env.step(action)
 	env.render()
+
 	print("S: ", state, "Action: ", action, "Reward: ", reward, "Info: ",info)
 
 	if done:
--- lab5-2.py
+++ lab5-2.py
@@ -1,16 +1,21 @@
-# Non-deterministic
+# Non-deterministic world
 
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+# FrozenLake-v0 는 기본적으로 slippery가 On 되어있음, 게임 환경 생성
 env = gym.make('FrozenLake-v0')
+
+# 게임 정보를 저장할 Q 테이블 생성 16x4
 Q = np.zeros([env.observation_space.n,env.action_space.n])
 
-learning_rate = .85
-dis = .99
+# Hyperparamters
+learning_rate = .85 # 학습률
+dis = .99 # Discount factor
 num_episodes = 2000
 
+# 성공 여부들을 저장(몇번 성공했는지 체크하기 위함)
 rList = []
 
 for i in range(num_episodes):
@@ -19,20 +24,26 @@ for i in range(num_episodes):
     done = False
 
     while not done:
+        # Random Noise 
         action = np.argmax(Q[state,:] + np.random.randn(1,env.action_space.n)/(i+1))
 
         new_state, reward, done, _ = env.step(action)
         
+        # Non-deterministic and Discounted reward Q-learning, update table
         Q[state,action] = (1-learning_rate) * Q[state,action] \
             + learning_rate*(reward+dis * np.max(Q[new_state,:]))
 
+        # rAll이 0보다 크면 성공
         rAll += reward
         state = new_state
 
+    # 성공여부를 리스트에 저장
     rList.append(rAll)
 
 print("Score over time: " + str(sum(rList)/num_episodes))
 print("Final Q-Table Values")
 print(Q)
+
+# 학습 결과를 그래프로 출력
 plt.bar(range(len(rList)),rList,color="blue")
 plt.show()
